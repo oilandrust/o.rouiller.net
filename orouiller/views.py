d90a67722661e20bb3a95ef01bf75347c0abaeb2
olivier <o.rouiller@gmail.com>
2015-02-03 13:27:32 +0100

    splited the old project list in two

--- orouiller/views.py
+++ orouiller/views.py
@@ -5,14 +5,17 @@ def home(request):
 
     projects = Project.objects.exclude(type='publication')
     recent = projects.exclude(old=True)
-    old = projects.filter(old=True)
 
+    old_list = list(projects.filter(old=True))
+    old_list_1 = old_list[::2]
+    old_list_2 = old_list[1::2]
 
     publications = Project.objects.filter(type='publication')
 
     return render(request, 'orouiller/home.html', 
                             {
                                 'projects':recent,
-                                'old_projects':old,
+                                'old_projects_1':old_list_1,
+                                'old_projects_2':old_list_2,
                                 'publications':publications,
                             })
